4ee55600d12549beb3c7262a6e1bd06a6e5d9ea7
Whitney <White.Whitney543@gmail.com>
2020-06-30 22:20:06 -0600

    updating

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,4 +5,9 @@ import csv
 csv_path = os.path.join("Resources", "Budget_Data.csv")
 #open file
 with open(csv_path, nextline) as csvfile:
+    csvreader =csvreader(csvfile, delimeter=",")
+    csv_header = next(csvfile)
     
+    print(f"header: {Finnancial Analysis}")
+    
+    #Find Profit and Loss
