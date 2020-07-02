e04350bf23d77ae3960de61bf17ee2832afb5848
Whitney <White.Whitney543@gmail.com>
2020-07-01 18:13:52 -0600

    updating homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,4 +10,25 @@ with open(csv_path, nextline) as csvfile:
     
     print(f"header: {Finnancial Analysis}")
     
-    #Find Profit and Loss
+    #Find Profit, Loss and how many months
+    Profit = []
+    Loss = []
+    Months = []
+    
+    # each row count
+    Profit.append(row[2])
+    Months.append(row[1])
+    
+    #find profit/loss change
+    profit_change = []
+    loss_change = []
+    
+    for x in range(1, len(Profit)):
+        profit_change.append((int(Profit[x])- int(Profit[x-1])))
+        
+    #average profit change
+        profit_change = sum(profit_change) / len(profit_change)
+        
+    #total months 
+    total_months = len(months)
+    
