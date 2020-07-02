0dd0a7902dc544d5407d793e3bc2bf5d2490c1b4
Whitney <White.Whitney543@gmail.com>
2020-07-01 18:34:34 -0600

    updating homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -27,8 +27,30 @@ with open(csv_path, nextline) as csvfile:
         profit_change.append((int(Profit[x])- int(Profit[x-1])))
         
     #average profit change
-        profit_change = sum(profit_change) / len(profit_change)
+       average profit_change = sum(profit_change) / len(profit_change)
         
     #total months 
     total_months = len(months)
+    
+    #increase/decrease
+    greatest_increase = max(profit_change)
+    greatest_decrease = min(profit_change)
+    
+    #Find Analysis
+    print("Finnancial Analysis")
+    
+    print(".................................................................")
+    
+    print("total months" + str(total_months))
+    
+    print("average change" + str(profit_change))
+    
+    print("greatest increase in profit" + str(months[profit_change.index(max(profit_change))+2]) + str(greatest_increase)
+    
+    print("greatest_decrease in profit" + str(months[profit_change.index(min(profit_change))+2]) + str(greatest_decrease)
+          
+                                                     
+                    
+                                                                         
+    
     
