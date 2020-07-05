4b46b010c10de4840d358d7de58a9e236a7c89f8
Whitney <White.Whitney543@gmail.com>
2020-07-04 23:57:21 -0600

    homework

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,22 +1,2 @@
-import os
-import csv
 
-#add path
-csv_path = os.path.join("Resources", "Election_Data.csv")
-#open file
-with open(csv_path, nextline) as csvfile:
-    csvreader =csvreader(csvfile, delimeter=",")
-    csv_header = next(csvfile)
     
-    print(f"header": "Election Results")
-    
-    #find total votes, percentage of votes per candiate, winner
-    total votes = []
-    percentage votes Khan = []
-    percentage votes Correy = []
-    percentage votes Li = []
-    percentage votes O'Tooley = []
-    Winner = []
-    
-    #total votes
-    total_votes = len(votes)
